dcfff2a01af2389123af348a35ef1ac1cc0e29ce
kinleygyeltshen5176 <kinleygyeltshen5176@gmail.com>
2024-12-09 10:01:28 +0600

    BBSF_02: Added signup functionality

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      progressBar: true, // Optional: adds a progress bar
+      closeButton: true  // Optional: adds a close button
+    })
+  ]
 };
