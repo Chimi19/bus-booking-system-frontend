634fae11a53328dd522534a87bbf08be84b79f45
rinchen1789 <rinchen1788@gmail.com>
2024-12-19 20:55:31 +0600

    BBSF-09:Added navbar and dashboard (#8)

---
+++ src/app/addbus/addbus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addbus',
+  standalone: true,
+  imports: [],
+  templateUrl: './addbus.component.html',
+  styleUrl: './addbus.component.css'
+})
+export class AddbusComponent {
+
+}
