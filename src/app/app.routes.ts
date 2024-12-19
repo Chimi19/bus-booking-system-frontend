86120d80a2fbf32d4219a8eebafe545c7fb82ffa
rinchen1789 <rinchen1788@gmail.com>
2024-12-19 20:36:04 +0600

    BBSF-09:Added navbar and dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,31 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddbusComponent } from './addbus/addbus.component';
+import { DashboardContentComponent } from './admin/dashboard-content/dashboard-content.component';
 
 export const routes: Routes = [
-    { path: '', component: SignupComponent }, // Default route
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: SignupComponent }, // Default route
+{ path: 'signup', component: SignupComponent },
+{ path: 'login', component: LoginComponent },
+
+{ 
+  path: 'dashboard', 
+  component: DashboardComponent, 
+  children:[
+    {
+      path: '',
+      component: DashboardContentComponent
+    },
+    {
+      path: 'dashboard-content',
+      component: DashboardContentComponent
+    },
+    {
+      path: 'addbus',
+      component: AddbusComponent
+    }
+  ]
+},
+
 ];
