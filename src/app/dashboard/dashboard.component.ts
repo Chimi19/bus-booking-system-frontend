b17ce2ab16d04c91b22d5793cb7bec3d1f35d2f4
kinley <152841520+kingyel123@users.noreply.github.com>
2024-12-20 14:31:02 +0600

    BBSF-10:Implemented logout and updated dashboard (#9)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,49 @@
 
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../services/auth.service';
+import { ProfileDropdownComponent } from "../profile-dropdown/profile-dropdown.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink,RouterOutlet],
+  imports: [RouterLink, RouterOutlet, ProfileDropdownComponent,CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  standalone: true,
 })
 export class DashboardComponent {
+
+  currentUser$: Observable<User | null>;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private toastr: ToastrService
+  ) {
+    this.currentUser$ = this.authService.getCurrentUser();
+  }
+
+  toggleSidebar(): void {
+    // Implement sidebar toggle logic
+    console.log('Toggle Sidebar');
+  }
+
+  handleLogout(): void {
+    this.authService.logout();
+    this.toastr.success('Logout successful', 'Success');
+    this.router.navigate(['/login']);
+  }
+
+  navigateToProfile(): void {
+    this.router.navigate(['/profile']);
+  }
+
+  navigateToSettings(): void {
+    this.router.navigate(['/settings']);
+  }
 
 }
