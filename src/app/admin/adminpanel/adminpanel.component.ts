b0fe9742dcf04bfdc45936197de763ba389d2b4e
rinchen1789 <rinchen1788@gmail.com>
2024-12-27 13:24:43 +0600

    BBSF-17: Implemented Admin Panel

---
+++ src/app/admin/adminpanel/adminpanel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-adminpanel',
+  standalone:true,
+  imports: [],
+  templateUrl: './adminpanel.component.html',
+  styleUrl: './adminpanel.component.css'
+})
+export class AdminpanelComponent {
+
+}
