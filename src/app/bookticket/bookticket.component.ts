262dbe650290411f2210677bdad2815ff6a60041
Jigme Gee <jigme2128@gmail.com>
2024-12-27 10:15:50 +0600

    BBSF-16:Generated ticket component and added button functionality

---
+++ src/app/bookticket/bookticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookticket',
+  imports: [],
+  templateUrl: './bookticket.component.html',
+  styleUrl: './bookticket.component.css'
+})
+export class BookticketComponent {
+
+}
